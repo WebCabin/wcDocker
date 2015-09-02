d51b8b5f7c3328c20ef0caae2dc5ea6dee13c488
mc007 <asd28071977>
2015-09-02 02:15:31 +0200

    =auto-commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,69 @@ module.exports = function (grunt) {
         cb();
     }
 
+    var outprop = "amdoutput";
+    var outdir = "./build/";
+    var tmpdir = "./tmp/";
+
     // Project configuration.
     grunt.initConfig({
+        amdloader: {
+            baseUrl: ".",
+
+            paths: {
+                jquery: "bower_components/jquery",
+                lie: "bower_components/lie",
+                "requirejs-dplugins": "../../.."
+            },
+
+            // Unfortunately this is needed for the jquery plugin.
+            // It's automatically handled by the plugin itself at runtime, but not during builds.
+            map: {
+                "*": {
+                    "jquery/src/selector": "jquery/src/selector-native"
+                }
+            }
+        },
+        amdbuild: {
+            dir: tmpdir,
+
+            // List of layers to build.
+            layers: [
+                // Test build for jquery plugin.  Should contain main test js file and a few jquery modules.
+                {
+                    name: "app",
+                    include: [
+                        // Modules and layers listed here, and their dependencies, will be added to the layer.
+                        "src"
+                    ]
+                }
+            ]
+        },
+        // Config to allow to concatenate files to generate the layer.
+        concat: {
+            options: {
+                banner: "<%= " + outprop + ".header%>",
+                sourceMap: true
+            },
+            dist: {
+                src: "<%= " + outprop + ".modules.abs %>",
+                dest: outdir + "<%= " + outprop + ".layerPath %>"
+            }
+        },
+
+        copy: {
+            plugins: {
+                expand: true,
+                cwd: tmpdir,
+                src: "<%= " + outprop + ".plugins.rel %>",
+                dest: outdir
+            }
+        },
+
+        clean: {
+            out: [outdir],
+            temp: [tmpdir]
+        },
         pkg: grunt.file.readJSON("package.json"),
         jshint: {
             src: [
@@ -197,6 +258,12 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-contrib-less");
     grunt.loadNpmTasks("grunt-contrib-uglify");
+
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-amd-build");
+
     grunt.loadNpmTasks("jsdoc-amddcl");
 
     grunt.loadNpmTasks('intern-geezer');
@@ -210,5 +277,20 @@ module.exports = function (grunt) {
     grunt.registerTask('services', [
         'shell:deviceServer',
         'shell:xide'
+    ]);
+
+    grunt.registerTask("amdbuild", function (amdloader) {
+        var name = this.name, layers = grunt.config(name).layers;
+        layers.forEach(function (layer) {
+            grunt.task.run("amddepsscan:" + layer.name + ":" + name + ":" + amdloader);
+            grunt.task.run("amdserialize:" + layer.name + ":" + name + ":" + outprop);
+            grunt.task.run("concat");
+            grunt.task.run("copy:plugins");
+        });
+    });
+    grunt.registerTask("build", [
+        "clean:out",
+        "clean:temp",
+        "amdbuild:amdloader"
     ]);
 };
