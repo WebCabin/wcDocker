86a7ec59afbf58c318dd6db0aa97bb0ace95d664
mc007 <asd28071977>
2015-09-04 12:28:17 +0200

    =removed createDocs.sh

--- createDocs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p Build/Docs
-rm -rf Build/Docs
-mkdir -p Build/Docs
-./node_modules/.bin/jsdoc Code README.md -u Code/tutorials -t Compiler/node_modules/ink-docstrap/template -c Compiler/config_documents.json -d Build/Docs
