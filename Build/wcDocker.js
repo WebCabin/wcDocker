943ee42f7a6124120969b77b58b019989d1e67c8
Denis Anchugov <denis.anchugov@outlook.com>
2018-02-02 18:35:10 +0100

    Bug fix for https://github.com/WebCabin/wcDocker/issues/108. Compiled correctly.

--- Build/wcDocker.js
+++ Build/wcDocker.js
@@ -1,5 +1,4 @@
-(function () {
-/**
+(function () {/**
  * @license almond 0.3.1 Copyright (c) 2011-2014, The Dojo Foundation All Rights Reserved.
  * Available via the MIT or new BSD license.
  * see: http://github.com/jrburke/almond for details
